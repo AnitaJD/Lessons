e4b4fd56a60dc8b08be5d9d76adcdccb77b5b10c
Anita <y_anita@mail.ru>
2019-02-22 17:16:29 +0300

    Homework

---
+++ Lesson_2_Arrays/src/main/java/Homework2.java
@@ -0,0 +1,38 @@
+public class Homework2 {
+
+    private int [] arr;
+    private int size;
+
+    public Homework2(int size) {
+        this.size = 0;
+        this.arr = new int [size];
+    }
+
+    public void delete (int value){
+        int i = 0;
+        for (i = 0; i < this.size ; i++) {
+            if (this.arr[i] == value){
+                break;
+            }
+
+            for (int j = i; j < this.size - 1 ; j++) {
+                this.arr[j] = this.arr [j + 1];
+            }
+            this.size--;
+        }
+    }
+
+    public void insert(int value){
+        int i;
+        for(i = 0; i < this.size; i++) {
+            if (this.arr[i] > value) {
+                break;
+            }
+        }
+            for (int j = this.size; j > i; j--){
+                this.arr[j] = this.arr[j - 1];
+            }
+            this.arr[i] = value;
+            this.size++;
+    }
+}
--- Lesson_2_Arrays/src/main/java/Main.java
+++ Lesson_2_Arrays/src/main/java/Main.java
@@ -1,7 +1,11 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-
+        Homework2 arr = new Homework2 (10_000);
+        Random r = new Random();
+        arr.insert(r.nextInt());
 
     }
 }
