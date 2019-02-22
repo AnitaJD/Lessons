753b2cd5a4624c164e06fa5e6304eace4b3c7499
Anita <y_anita@mail.ru>
2019-02-22 17:58:26 +0300

    Homework

--- Lesson_2_Arrays/src/main/java/Main.java
+++ Lesson_2_Arrays/src/main/java/Main.java
@@ -3,9 +3,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        Homework2 arr = new Homework2 (10_000);
-        Random r = new Random();
-        arr.insert(r.nextInt());
+        Homework2 arr = new Homework2 (3);
 
     }
 }
