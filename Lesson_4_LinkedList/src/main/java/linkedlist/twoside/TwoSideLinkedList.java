ed9d305fd6753b0341fcff1a51bdf6e2da23604b
Anita <y_anita@mail.ru>
2019-02-26 17:23:16 +0300

    Lesson

---
+++ Lesson_4_LinkedList/src/main/java/linkedlist/twoside/TwoSideLinkedList.java
@@ -0,0 +1,11 @@
+package linkedlist.twoside;
+
+import linkedlist.LinkedList;
+
+public interface TwoSideLinkedList <E> extends LinkedList <E> {
+
+    void insertLeft (E value);
+    void insertRight (E value);
+
+    E removeLeft();
+}
